93fbf8c38178d6c78d87d823130a49fe53c97f39
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2021-01-30 18:41:35 +0530

    stream input & camSetup
    Def config:-
    • res: 500x500
    • audio = false
    • video.width = videoWidth; &&  video.height = videoHeight;

---
+++ js/game.js
@@ -0,0 +1,37 @@
+const videoWidth = 500;
+const videoHeight = 500;
+const video = document.getElementById('myVideo');
+
+async function setupCamera() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        throw new Error(
+            'Browser API navigator.mediaDevices.getUserMedia is not available');
+    }
+
+    video.width = videoWidth; // Eta basically object & subject body k same frame e impose korche
+    video.height = videoHeight;
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+        'audio': false,
+        'video': {
+        facingMode: 'user',
+        width: videoWidth,
+        height: videoHeight,
+        },
+    });
+    video.srcObject = stream;
+
+    return new Promise((resolve) => {
+        video.onloadedmetadata = () => {
+        resolve(video);
+        };
+    });
+}
+
+async function loadVideo() {
+    const video = await setupCamera();
+    video.play();
+    return video;
+}
+
+loadVideo();
