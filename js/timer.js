4d684723639679a8d69b84613cd72c5c23e3b7c0
Sandipan Dey <sandipandey.12@gmail.com>
2021-01-31 18:49:30 +0530

    toggle switch

---
+++ js/timer.js
@@ -0,0 +1,8 @@
+// Pore add korbo
+
+// this is the code for dark mode
+let input = document.querySelector('#toggle');
+input.addEventListener('click', () => {
+  document.body.classList.toggle('switch');
+
+})
